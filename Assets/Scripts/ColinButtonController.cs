c2d94f9f1cc23e5d488fbbef7b74379612cb55d1
marcusdaly <mrd109@case.edu>
2019-05-07 22:37:25 -0400

    Make button prefab

---
+++ Assets/Scripts/ColinButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ColinButtonController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent buttonEvent;
+    [SerializeField] private string buttonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndBehavior end = gameObject.GetComponentInChildren<EndBehavior>();
+        end.onColinCollision = buttonEvent;
+
+        Text text = gameObject.GetComponentInChildren<Text>();
+        text.text = buttonText;
+    }
+}
